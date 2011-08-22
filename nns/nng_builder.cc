453b9646454f6f3955f5f623873cfe803c44fe41
shnya <testmhigashi@gmail.com>
2011-08-22 23:21:06 +0900

    commit

--- nns/nng_builder.cc
+++ nns/nng_builder.cc
@@ -12,14 +12,20 @@ int main(int argc, char *argv[])
   if(argc < 2) return -1;
   //cout << "begin index" << endl;
   LineReader::make_index(argv[1], (string(argv[1]) + ".idx").c_str());
-  LineReader reader(argv[1], (string(argv[1]) + ".idx").c_str());
+  //LineReader reader(argv[1], (string(argv[1]) + ".idx").c_str());
+  std::ifstream ifs(argv[1]);
+  vector<string> lines;
+  string line;
+  while(getline(ifs,line)){
+    lines.push_back(line);
+  }
   //cout << "made index" << endl;
-  NNGraph<LineReader, LevenshteinSimilarity> g(reader,15);
-  for(size_t i = 0; i < reader.size(); i++){
+  NNGraph<vector<string>, LevenshteinSimilarity> g(lines,15);
+  for(size_t i = 0; i < lines.size(); i++){
     std::vector<std::pair<int,float> > nn = g.get_nn(i);
-    cout << reader[i] << "\t";
+    cout << lines[i] << "\t";
     for(size_t j = 0; j < nn.size(); j++){
-      cout << reader[nn[j].first] << "," << nn[j].second << " ";
+      cout << lines[nn[j].first] << "," << nn[j].second << " ";
     }
     cout << endl;
   }
